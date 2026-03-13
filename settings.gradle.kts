e8c5f3f0aa9e1cf9c5f3ea4034e2e3a8261e48c5
skupie <114020227+skupie@users.noreply.github.com>
2026-03-13 17:05:00 +0600

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,6 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -16,8 +10,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
-
-rootProject.name = "StudentPortal"
+rootProject.name = "RadiantApp"
 include(":app")
